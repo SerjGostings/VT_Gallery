bd98566cd6086bc269efe823c6ce010b88f3fa1b
SerjGostings <49344941+SerjGostings@users.noreply.github.com>
2021-12-18 11:15:29 +0300

    Add files via upload

---
+++ Gallery/scripts/script.js
@@ -0,0 +1,25 @@
+function swap(img)
+{
+    var tmp = document.images[0].src
+    document.images[0].src=img.src
+    img.src = tmp
+}
+position = 4;
+function left()
+{
+    position --;
+    if (position < 1)
+    {
+        position = 4
+    }
+    document.images[0].src="../img/img_"+position+".jpg"
+}
+function right()
+{
+    position ++;
+    if (position > 4)
+    {
+        position = 1
+    }
+    document.images[0].src="../img/img_"+position+".jpg"
+}
